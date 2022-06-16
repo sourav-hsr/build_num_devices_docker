b4197e8dd9a882018dd5961ee146e57ccf3fc15d
sourav-hsr <sourav@healthsolutionsresearch.org>
2022-06-16 15:48:49 -0500

    Add lambda files

---
+++ app.py
@@ -0,0 +1,45 @@
+import time
+import boto3
+
+#query = "SELECT * FROM ('stg-authdatadb'.sgcbgcensus)"
+
+
+
+DATABASE = 'stg-authdatadb'
+TABLE='sgcbgcensus'
+TABLE2='home_panel_summary'
+
+output='s3://stg-hsr-athena/bri-refactor/Sourav_test_athena/docker_github_actions_results/'
+
+def lambda_handler(event, context):
+    
+    #STATE=event['state']
+    #DATE=event['date']
+    
+    #print(STATE)
+    #print(DATE)
+
+    #query = """ SELECT * FROM "stg-authdatadb"."sgcbgcensus" """
+    query=("SELECT b.number_devices_residing,"
+    "b.census_block_group,"
+    "c.b01001e1, "
+    "b.date_range_start "
+    f"""FROM "{DATABASE}"."{TABLE}" c """
+    f"""LEFT OUTER JOIN "{DATABASE}"."{TABLE2}" b ON lpad(replace(c.census_block_group, '.0'), 12, '0') = lpad(replace(replace(b.census_block_group, 'CA:',''), '.0', ''), 12, '0') """
+    "WHERE b.region = 'md' "
+    "and lpad(b.date_range_start, 10, '0') = '2021-06-28'")
+    
+    print(query)
+
+    client = boto3.client('athena')
+    # Execution
+    response = client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': DATABASE
+        },
+        ResultConfiguration={
+            'OutputLocation': output,
+        }
+    )
+    return response
